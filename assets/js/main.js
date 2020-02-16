543b9c8187cde3b2fb78ed1e63379f7e0ecdca9e
Philippe Baurens <pbaurens.dev@gmail.com>
2020-02-16 02:58:04 +0100

    navbar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,6 +51,13 @@ console.log( 'Hello + input value )*/
 
 /*--------------CHECK MY WORK Portfolio-------------*/
 
+/*const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click' , () => {
+  navbarLinks.classList.toggle('active')
+})*/
+
 
 
 
